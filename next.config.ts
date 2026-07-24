f6324cb14f23cdd72f5c80151c26b48ccb45fe8c
Mubix Labs <mubixlabs@gmail.com>
2026-07-24 00:24:26 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    optimizePackageImports: ["lucide-react", "framer-motion", "react-icons"],
+  },
 
-export default nextConfig;
+};
+export default nextConfig
