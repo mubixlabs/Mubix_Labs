096aca92d44390fec613b420173345494b48bdda
Mubix Labs <mubixlabs@gmail.com>
2026-07-27 08:01:24 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,6 +100,15 @@ export default function RootLayout({
     </Script>
   </>
 )}
+        {/* Umami Analytics */}
+        {process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+          <Script
+            async
+            src="https://cloud.umami.is/script.js"
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="afterInteractive"
+          />
+        )}
 </body>
     </html>
   );
